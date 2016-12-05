305b10f4a58a6b5a892463739451da4d333ee572
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2016-12-05 05:00:21 +0000

    existing files

---
+++ WebLedServer/main.js
@@ -0,0 +1,32 @@
+\express = require('express');  //web server
+app = express();
+server = require('http').createServer(app);
+io = require('socket.io').listen(server);	//web socket server
+
+server.listen(8080); //start the webserver on port 8080
+app.use(express.static('public')); //tell the server that ./public/ contains the static webpages
+
+var SerialPort = require("serialport");
+var serialPort = new SerialPort("/dev/ttyACM0", { baudrate: 115200 });
+
+var sat = 0;
+var hue = 0;
+var brt = 0;
+
+io.sockets.on('connection', function (socket) { //gets called whenever a client connects
+    socket.emit('led', {value: brightness}); //send the new client the current brightness
+
+    socket.on('led', function (data) { //makes the socket react to 'led' packets by calling this function
+        brightness = data.value;  //updates brightness from the data object
+        var buf = new Buffer(3); //creates a new 3-byte buffer
+        buf.writeUInt8(brightness, 0); //writes the pwm value to the buffer
+	buf.writeUInt8(brightness, 1);
+	buf.writeUInt8(brightness, 2);
+        serialPort.write(buf); //transmits the buffer to the arduino
+
+
+        io.sockets.emit('led', {value: brightness}); //sends the updated brightness to all connected clients
+    });
+});
+
+console.log("running");

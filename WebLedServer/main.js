a7df55f0f0cf05bfe4b97a79126278d0543a73cb
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2017-01-19 23:51:54 -0500

    test

--- WebLedServer/main.js
+++ WebLedServer/main.js
@@ -16,30 +16,30 @@ var version = 1;
 var color = "#000000";
 
 app.head("/notification",function(request, response){
-  // Turn off led's at T-0
-  serialPort.write("m");
-  serialPort.write("0");
-
-  // Flash them back at T+200ms
-  setTimeout(function() {
-    serialPort.write("m");
-    serialPort.write("1");
-}, 200);
-
-  // Flash off at T+400ms
-  setTimeout(function() {
+    // Turn off led's at T-0
     serialPort.write("m");
     serialPort.write("0");
-}, 400);
 
-  // Return to previously scheduled programming at T+600ms
-  setTimeout(function() {
-    serialPort.write("m");
-    serialPort.write((Number(mode)+Number(version)-1).toString());
-}, 600);
+    // Flash them back at T+200ms
+    setTimeout(function() {
+        serialPort.write("m");
+        serialPort.write("1");
+    }, 200);
 
-  response.writeHead(200, {"Content-Type": "application/json"});
-  response.end();
+    // Flash off at T+400ms
+    setTimeout(function() {
+        serialPort.write("m");
+        serialPort.write("0");
+    }, 400);
+
+    // Return to previously scheduled programming at T+600ms
+    setTimeout(function() {
+        serialPort.write("m");
+        serialPort.write((Number(mode)+Number(version)-1).toString());
+    }, 600);
+
+    response.writeHead(200, {"Content-Type": "application/json"});
+    response.end();
 });
 
 io.sockets.on('connection', function (socket) { //gets called whenever a client connects
@@ -47,26 +47,26 @@ io.sockets.on('connection', function (socket) { //gets called whenever a client 
 
     socket.on('led', function (data) { //makes the socket react to 'led' packets by calling this function
 
-        console.log("Receved data:\n");
-        console.log(data);
-        modeName = data.mode;
+    console.log("Receved data:\n");
+    console.log(data);
+    modeName = data.mode;
 
-        if (data.mode == "allOff") mode = 0;
-        else if (data.mode == "allOn") mode = 1;
-        else if (data.mode == "twinkle") mode = 2;
-        else if (data.mode == "twoSin") mode = 3;
-        else if (data.mode == "matrix") mode = 13;
-        else if (data.mode == "oneSin") mode = 16;
-        else if (data.mode == "popFade") mode = 22;
-        else if (data.mode == "threeSin") mode = 28;
-        else if (data.mode == "rainbow") mode = 31;
-        else if (data.mode == "noise") mode = 37;
-        else if (data.mode == "confetti") mode = 39;
-        else if (data.mode == "sinelon") mode = 40;
-        else if (data.mode == "juggle") mode = 41;
-        else {
-            console.log("Received bad data.");
-            return;}
+    if (data.mode == "allOff") mode = 0;
+    else if (data.mode == "allOn") mode = 1;
+    else if (data.mode == "twinkle") mode = 2;
+    else if (data.mode == "twoSin") mode = 3;
+    else if (data.mode == "matrix") mode = 13;
+    else if (data.mode == "oneSin") mode = 16;
+    else if (data.mode == "popFade") mode = 22;
+    else if (data.mode == "threeSin") mode = 28;
+    else if (data.mode == "rainbow") mode = 31;
+    else if (data.mode == "noise") mode = 37;
+    else if (data.mode == "confetti") mode = 39;
+    else if (data.mode == "sinelon") mode = 40;
+    else if (data.mode == "juggle") mode = 41;
+    else {
+        console.log("Received bad data.");
+        return;}
         version = data.version;
 
         if (color != data.color){
@@ -78,17 +78,20 @@ io.sockets.on('connection', function (socket) { //gets called whenever a client 
             console.log("Writing 'b'");
             serialPort.write("b");
             console.log("Writing '"+(colorTiny.v * 255).toString()+"'");
-            serialPort.write((colorTiny.v * 255).toString());
+            serialPort.write("255")
+            //serialPort.write((colorTiny.v * 255).toString());
 
             console.log("Writing 'h'");
             serialPort.write("h");
             console.log("Writing '"+(colorTiny.h * 255).toString()+"'");
-            serialPort.write((colorTiny.h * 255).toString());
+            serialPort.write("130")
+            //serialPort.write((colorTiny.h * 255).toString());
 
             console.log("Writing 't'");
             serialPort.write("t");
             console.log("Writing '"+(colorTiny.s * 255).toString()+"'");
-            serialPort.write((colorTiny.s * 255).toString());
+            serialPort.write("255")
+            //serialPort.write((colorTiny.s * 255).toString());
         }
 
         console.log("Writing 'm'");

93339c7a15dd94e116e1f088fc00855373db32a3
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2017-02-20 14:10:25 -0500

    Fixed cylon mode to be symmetric (mabye)

--- Room/src/sinelon.h
+++ Room/src/sinelon.h
@@ -5,9 +5,21 @@
 
 void sinelon() {                                              // a colored dot sweeping back and forth, with fading trails
   fadeToBlackBy( leds, NUM_LEDS, 20);
-  int pos = beatsin16(13,0,NUM_LEDS);
-  leds[pos] += CHSV( thishue, 255, 192);
+  int leftpos = beatsin16(13,0,LEFTNO);
+  int rightpos = beatsin16(13,0,RIGHTNO) + FRONTNO + LEFTNO;
+  int frontpos = beatsin16(13,0,FRONTNO) + LEFTNO;
+  int backpos = beatsin16(13,0,BACKNO) + LEFTNO + FRONTNO + RIGHTNO;
+  leds[leftpos] += CHSV( thishue, 255, 192);
+  leds[rightpos] += CHSV( thishue, 255, 192);
+  leds[frontpos] += CHSV( thishue, 255, 192);
+  leds[backpos] += CHSV( thishue, 255, 192);
   thishue++;
 }
+
+/*
+
+Note that we have to edit this because we want a cylon on EACH side of the room, not just on the whole strip!
+
+*/
 
 #endif

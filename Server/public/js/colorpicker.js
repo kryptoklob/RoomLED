f8425c82da4e5805028b4fc8f739df7ae4acf0a5
Caleb Ditchfield <caleb@neadwerx.com>
2017-08-09 14:25:17 -0400

    Removing old files

--- Server/public/js/colorpicker.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-
-  $("#flat").spectrum({
-      flat: true,
-      showInput: true
-  });
-});
--- Server/public/js/videoStatus.js
+++
@@ -1,9 +0,0 @@
-// Query and set status text for video stream.
-
-$('video source').last().on('error', function() {
-    $("#videoStatus").text("Video (Status: Down)");
-});
-
-$('video').last().on('loadeddata', function() {
-    $("#videoStatus").text("Video (Status: Up)");
-})

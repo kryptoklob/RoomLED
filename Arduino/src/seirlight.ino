db24e1b514e257921fe43abafe6c00d485ad5552
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2017-12-20 16:36:51 -0500

    bugfix

--- Arduino/src/seirlight.ino
+++ Arduino/src/seirlight.ino
@@ -95,7 +95,7 @@ void strobe_mode(uint8_t newMode, bool mc){
 
     // 1 - all on
     case  1:
-      if(mc) { fill_solid(leds, NUM_LEDS_PER_STRIP, CHSV(160, 255, 255)); }
+      if(mc) { fill_solid(leds, NUM_LEDS_PER_STRIP, CHSV(160, 255, 128)); }
       break;
 
     // 2 - two-sin
